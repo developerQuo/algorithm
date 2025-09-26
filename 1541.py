4c34820841ddf75533c19cdf54f28f123bc228e7
changmin <developerQuo@gmail.com>
2025-09-26 16:58:49 +0900

    백준 1541

--- 1541.py
+++ 1541.py
@@ -3,71 +3,60 @@
 조건: 식은 ‘0’~‘9’, ‘+’, 그리고 ‘-’만으로 이루어져 있고, 가장 처음과 마지막 문자는 숫자이다. 그리고 연속해서 두 개 이상의 연산자가 나타나지 않는다.
 
 Reduction
-1. 숫자와 연산자 스택을 각각 만든다.
-2. 숫자와 연산자를 pop하면서 '+' 연산자가 나오면 계산해서 숫자 스택에 넣는다.
-3. '-' 연산자가 나오면 pop한 숫자를 음수로 바꾼다.
+1. - 연산자를 기준으로 나눈다.
+2. 각 원소를 모두 계산한다.
+3. 첫째 원소에서 나머지를 모두 빼준다.
+
 
 Pseudo Code
-num_stack = deque([])
-operator_stack = deque([])
 
-num_str = ""
-문자열을 리스트로 변경해서 반복:
-    문자가 '+' or "-"이면:
-        num_str을 숫자로 바꿔서 num_stack에 추가
-        연산자를 operator_stack에 추가
-        num_str을 빈문자열로 초기화
-    숫자면:
-        num_str에 추가
+num_segments = [ list(segment) for segment in str.split('-')]
+sum_nums = []
 
-def calculator():
-    연산자 스택에 원소가 있다면 반복:
-        연산자 스택에서 연산자를 꺼낸다.
-        연산자가 '+'이면:
-            numstack.append(num_stack.pop() + num_stack.pop())
-        연산자가 '-'이면:
-            numstack.append(-num_stack.pop())
+sum_segment = 0
+num_segments 반복 -> nums:
 
-    num_stack.pop() 반환
+    num_str = ""
+    nums 반복 -> num:
+        문자가 '+'이면:
+            num_str을 숫자로 바꿔서 sum_segment에 합산
+        숫자면:
+            num_str에 추가
+    else:
+        num_str을 숫자로 바꿔서 sum_segment에 합산
 
-calculator()
+    sum_nums.append(-sum_segment)
+
+print(sum(sum_nums))
+
 10
 
 10-10-20
+
+55-50-40+30+20-10+5
 """
 
 import sys
-from collections import deque
 
-num_str_list = list(sys.stdin.readline().strip())
+num_str_list = sys.stdin.readline().strip()
 
-num_stack = deque([])
-operator_stack = deque([])
+segments = [list(segment) for segment in num_str_list.split("-")]
+sum_nums = []
 
-num_str = ""
+sum_local = 0
 
-for i in range(len(num_str_list)):
-    if num_str_list[i] in ["+", "-"]:
-        num_stack.append(int(num_str))
-        operator_stack.append(num_str_list[i])
-        num_str = ""
-    else:
-        num_str += num_str_list[i]
-else:
-    num_stack.append(int(num_str))
-
-
-def calculator():
-    while operator_stack:
-        operator = operator_stack.pop()
-        if operator == "+":
-            num_stack.append(num_stack.pop() + num_stack.pop())
-        elif operator_stack and operator_stack[-1] == "-":
-            num_stack.append(num_stack.pop() + num_stack.pop())
+for i in range(len(segments)):
+    num_str = ""
+    for literal in segments[i]:
+        if literal == "+":
+            sum_local += int(num_str)
+            num_str = ""
         else:
-            num_stack.append(-num_stack.pop() + num_stack.pop())
+            num_str += literal
+    else:
+        sum_local += int(num_str)
 
-    return num_stack.pop()
+    sum_nums.append(-sum_local if i != 0 else sum_local)
+    sum_local = 0
 
-
-print(calculator())
+print(sum(sum_nums))
