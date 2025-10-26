ce2b7135946be4599faf1c52297fb296794a96dd
changmin <developerQuo@gmail.com>
2025-10-26 18:47:31 +0900

    백준 1439 - iteration

--- 1439.py
+++ 1439.py
@@ -10,106 +10,62 @@ Reduction
 3. 앞 문자열의 마지막 문자와 뒤 문자열의 첫 문자가 같으면 합친다.
 4. 더 이상 합칠 수 없으면 0, 1 카운터에 넣는다.
 
-11101101
+Data structure
+- stack
 
-1110 1101
+Pseudo code
+def iteration(literals: str):
+    counter = [0, 0]
+    stack에 문자열 넣기
 
-11 10 11 01
+    while(stack):
+        literal = stack.pop()
+        문자열을 10진수로 바꿨을 때, 결과가 0 or 2^(문자열 길이) - 1이면:
+            0, 1 카운트++
+        아니라면:
+            prev = stack.push(literal[0, 길이 / 2의 몫])
+            next = stack.push(literal[길이 / 2의 몫])
+            prev의 마지막 문자와 next의 첫 문자가 같으면:
+                0, 1 카운트--
 
-11 1 0 11 0 1
-
-11 10 11 01
-count 10 -> 2개
-count 01 -> 2개
-
-1110 1101
-count 1110 -> 같으니까 그대로 2개 유지
-count 1101 -> 다르니까 1개 추가해서 3개
-
-11101101
-count 11101101 -> 다르니까 1개 추가해서 4개
-
-
-
-0001100
-
-000 1100
-
-000 11 00
-
-000 1100
-count 1100 -> 2개
-
-0001100
-count 0001100 -> 3개
-
-
-11001100110011000001
-
-1100110011 0011000001
-
-11001 10011 00110 00001
-
-11 001 10 011 00 110 00 001
-
-11 0 01 10 0 11 00 1 10 00 0 01
-
-11 0 0 1 1 0 0 11 00 1 1 0 00 0 0 1
-
-11 0 01 10 0 11 00 1 10 00 0 01
-01 -> 0: 1, 1: 1
-10 -> 0: 2, 1: 2
-10 -> 0: 3, 1: 3
-
-
-Pseudo Code
-
-0 카운터
-1 카운터
-
-def recur(str literals) -> str:
-    문자열을 10진수로 바꿨을 때, 결과가 0 or 2^(문자열 길이) - 1이면:
-        0, 1 카운트++
-        문자열 반환
-    아니라면:
-        prev = recur(literals.slice(0, 길이 / 2의 몫))
-        next = recur(literals.slice(길이 / 2의 몫)))
-        prev의 마지막 문자와 next의 첫 문자가 같으면:
-            0, 1 카운트--
-        return prev + next
-
+    counter 중에 더 작은 것 출력
 """
 
 import sys
+from collections import deque
 
 literals = sys.stdin.readline().strip()
 
-zero_counter = 0
-one_counter = 0
 
+counter = [0, 0]
+stack = deque([literals])
+leaf_arr = []
 
-def recur(literals: str) -> str:
-    global zero_counter, one_counter
-    if int(literals, 2) in (0, 2 ** len(literals) - 1):
-        if literals[0] == "0":
-            zero_counter += 1
+while stack:
+    literal = stack.pop()
+    if int(literal, 2) in (0, 2 ** len(literal) - 1):
+        if literal[0] == "0":
+            counter[0] += 1
         else:
-            one_counter += 1
-        return literals
+            counter[1] += 1
+        leaf_arr.append(literal)
     else:
-        prev = recur(literals[0 : len(literals) // 2])
-        next = recur(literals[len(literals) // 2 :])
-        if prev[-1] == next[0]:
-            if next[0] == "0":
-                zero_counter -= 1
-            else:
-                one_counter -= 1
-        return prev + next
+        half = len(literal) // 2
+        stack.append(literal[0:half])
+        stack.append(literal[half:])
+
+prev_literal = ""
+for literal in leaf_arr:
+    if not prev_literal:
+        prev_literal = literal
+        continue
+
+    if literal[0] == prev_literal[-1]:
+        if literal[0] == "0":
+            counter[0] -= 1
+        else:
+            counter[1] -= 1
+    prev_literal = literal
 
 
-recur(literals)
-
-if zero_counter <= one_counter:
-    print(zero_counter)
-else:
-    print(one_counter)
+print(min(counter))
